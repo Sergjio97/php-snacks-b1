6750e59337e8cb17da0adcc5a0cded9cc8960e8d
Sergjio97 <sergiocizmja@gmail.com>
2022-01-18 16:37:32 +0100

    Snak 2

--- Snack 2/index.php
+++ Snack 2/index.php
@@ -1,4 +1,16 @@
 <?php
-/* Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
-che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+    /* Passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, 
+    che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato” */
+    // ?name=&mail=&age
+    //isset($_GET['name'])&&isset($_GET['mail'])&&isset($_GET['age'])
+    if (isset($_GET['name'])) {
+        $name = strlen($_GET['name']);
+    };
+
+    if ($name > 3 && strpos($_GET['mail'], '@') !== false && strpos($_GET['mail'], '.') !== false && is_numeric($_GET['age'])){
+        echo "Accesso riuscito";
+    } else {
+        echo "Accesso negato";
+    };
+
 ?>
